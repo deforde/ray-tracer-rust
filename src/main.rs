6ea5b84406c1abd166c3f5cc79708c15da419278
deforde <daniel.forde001@gmail.com>
2023-02-17 14:30:28 +0200

    add progress indicator

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() -> std::io::Result<()> {
     file.write_all(format!("P3\n{image_width} {image_height}\n255\n").as_bytes())?;
 
     for j in (0..image_height).rev() {
+        print!("\rscan lines remaining: {j}  ");
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
@@ -21,6 +22,7 @@ fn main() -> std::io::Result<()> {
             file.write_all(format!("{ir} {ig} {ib}\n").as_bytes())?;
         }
     }
+    println!("\ndone");
 
     Ok(())
 }
