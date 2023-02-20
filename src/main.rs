ca7ca7814511d91465822ddbfe3ed15957b5fd74
deforde <daniel.forde001@gmail.com>
2023-02-20 08:10:18 +0200

    implement shading using surface normals

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,20 @@ use vec::vec::Colour;
 use vec::vec::Point;
 use vec::vec::Vec;
 
-fn hit_sphere(centre: &Point, radius: f32, r: &Ray) -> bool {
+fn hit_sphere(centre: &Point, radius: f32, r: &Ray) -> f32 {
     let oc = r.orig.sub(&[*centre]);
-    let a = r.dir.dot(&r.dir);
-    let b = 2.0 * oc.dot(&r.dir);
-    let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    let a = r.dir.len_sqrd();
+    let b = oc.dot(&r.dir);
+    let c = oc.len_sqrd() - radius * radius;
+    let discriminant = b * b - a * c;
+    if discriminant < 0.0 {
+        return -1.0;
+    }
+    (-b - discriminant.sqrt()) / a
 }
 
 fn ray_colour(r: &Ray) -> Colour {
-    if hit_sphere(
+    let u = hit_sphere(
         &Point {
             x: 0.0,
             y: 0.0,
@@ -29,12 +32,22 @@ fn ray_colour(r: &Ray) -> Colour {
         },
         0.5,
         r,
-    ) {
+    );
+    if u > 0.0 {
+        let n = r
+            .at(u)
+            .sub(&[Vec {
+                x: 0.0,
+                y: 0.0,
+                z: -1.0,
+            }])
+            .unit();
         return Colour {
-            x: 1.0,
-            y: 0.0,
-            z: 0.0,
-        };
+            x: n.x + 1.0,
+            y: n.y + 1.0,
+            z: n.z + 1.0,
+        }
+        .mulf(0.5);
     }
     let unit = r.dir.unit();
     let t = 0.5 * (unit.y + 1.0);
