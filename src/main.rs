2820f63452f59b396d4ac5e96d1b781b42128481
deforde <daniel.forde001@gmail.com>
2023-02-20 15:24:17 +0200

    create random scene for rendering, update readme

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,11 @@ use metal::metal::Metal;
 use ray::ray::Ray;
 use sphere::sphere::Sphere;
 use util::util::rand_f32;
+use util::util::randmm_f32;
 use util::util::write_colour;
+use vec::vec::rand;
 use vec::vec::rand_hemisphere;
+use vec::vec::randmm;
 use vec::vec::Colour;
 use vec::vec::Point;
 use vec::vec::Vec;
@@ -112,103 +115,148 @@ fn ray_colour(r: &Ray, world: &HittableList, depth: i32) -> Colour {
     .mulf(t)])
 }
 
+fn random_scene() -> HittableList {
+    let mut world = HittableList {
+        objects: std::vec::Vec::new(),
+    };
+
+    let mat_gnd = Materials::Lambertian(Lambertian {
+        albedo: Colour {
+            x: 0.5,
+            y: 0.5,
+            z: 0.5,
+        },
+    });
+    world.objects.push(Hittables::Sphere(Sphere {
+        c: Point {
+            x: 0.0,
+            y: -1000.0,
+            z: 0.0,
+        },
+        r: 1000.0,
+        mat: mat_gnd,
+    }));
+
+    for i in -11..11 {
+        for j in -11..11 {
+            let choose = rand_f32();
+            let centre = Point {
+                x: i as f32 + 0.9 * rand_f32(),
+                y: 0.2,
+                z: j as f32 + 0.9 * rand_f32(),
+            };
+            if (centre.sub(&[Point {
+                x: 4.0,
+                y: 0.2,
+                z: 0.0,
+            }]))
+            .len()
+                > 0.9
+            {
+                if choose < 0.8 {
+                    let albedo = rand().mul(&[rand()]);
+                    let mat = Materials::Lambertian(Lambertian { albedo });
+                    world.objects.push(Hittables::Sphere(Sphere {
+                        c: centre,
+                        r: 0.2,
+                        mat,
+                    }));
+                } else if choose < 0.95 {
+                    let albedo = randmm(0.5, 1.0);
+                    let fuzz = randmm_f32(0.0, 0.5);
+                    let mat = Materials::Metal(Metal { albedo, fuzz });
+                    world.objects.push(Hittables::Sphere(Sphere {
+                        c: centre,
+                        r: 0.2,
+                        mat,
+                    }));
+                } else {
+                    let mat = Materials::Dielectric(Dielectric { ir: 1.5 });
+                    world.objects.push(Hittables::Sphere(Sphere {
+                        c: centre,
+                        r: 0.2,
+                        mat,
+                    }));
+                }
+            }
+        }
+    }
+
+    let mut mat = Materials::Dielectric(Dielectric { ir: 1.5 });
+    world.objects.push(Hittables::Sphere(Sphere {
+        c: Point {
+            x: 0.0,
+            y: 1.0,
+            z: 0.0,
+        },
+        r: 1.0,
+        mat,
+    }));
+
+    mat = Materials::Lambertian(Lambertian {
+        albedo: Colour {
+            x: 0.4,
+            y: 0.2,
+            z: 0.1,
+        },
+    });
+    world.objects.push(Hittables::Sphere(Sphere {
+        c: Point {
+            x: -4.0,
+            y: 1.0,
+            z: 0.0,
+        },
+        r: 1.0,
+        mat,
+    }));
+
+    mat = Materials::Metal(Metal {
+        albedo: Colour {
+            x: 0.7,
+            y: 0.6,
+            z: 0.5,
+        },
+        fuzz: 0.0,
+    });
+    world.objects.push(Hittables::Sphere(Sphere {
+        c: Point {
+            x: 4.0,
+            y: 1.0,
+            z: 0.0,
+        },
+        r: 1.0,
+        mat,
+    }));
+
+    return world;
+}
+
 fn main() -> std::io::Result<()> {
-    let aspect_ratio = 16.0 / 9.0;
+    let aspect_ratio = 3.0 / 2.0;
     let image_width = 400;
     let image_height = (image_width as f32 / aspect_ratio) as i32;
     let n_samples = 100;
     let max_depth = 50;
 
-    let mat_gnd = Materials::Lambertian(Lambertian {
-        albedo: Colour {
-            x: 0.8,
-            y: 0.8,
-            z: 0.0,
-        },
-    });
-    let mat_centre = Materials::Lambertian(Lambertian {
-        albedo: Colour {
-            x: 0.1,
-            y: 0.2,
-            z: 0.5,
-        },
-    });
-    let mat_left = Materials::Dielectric(Dielectric { ir: 1.5 });
-    let mat_right = Materials::Metal(Metal {
-        albedo: Colour {
-            x: 0.8,
-            y: 0.6,
-            z: 0.2,
-        },
-        fuzz: 0.0,
-    });
-
-    let mut world = HittableList {
-        objects: std::vec::Vec::new(),
-    };
-    world.objects.push(Hittables::Sphere(Sphere {
-        c: Point {
-            x: 0.0,
-            y: -100.5,
-            z: -1.0,
-        },
-        r: 100.0,
-        mat: mat_gnd,
-    }));
-    world.objects.push(Hittables::Sphere(Sphere {
-        c: Point {
-            x: 0.0,
-            y: 0.0,
-            z: -1.0,
-        },
-        r: 0.5,
-        mat: mat_centre,
-    }));
-    world.objects.push(Hittables::Sphere(Sphere {
-        c: Point {
-            x: -1.0,
-            y: 0.0,
-            z: -1.0,
-        },
-        r: 0.5,
-        mat: mat_left,
-    }));
-    world.objects.push(Hittables::Sphere(Sphere {
-        c: Point {
-            x: -1.0,
-            y: 0.0,
-            z: -1.0,
-        },
-        r: -0.45,
-        mat: mat_left,
-    }));
-    world.objects.push(Hittables::Sphere(Sphere {
-        c: Point {
-            x: 1.0,
-            y: 0.0,
-            z: -1.0,
-        },
-        r: 0.5,
-        mat: mat_right,
-    }));
+    let world = random_scene();
 
     let lf = Point {
-        x: 3.0,
-        y: 3.0,
-        z: 2.0,
+        x: 13.0,
+        y: 2.0,
+        z: 3.0,
     };
     let la = Point {
         x: 0.0,
         y: 0.0,
-        z: -1.0,
+        z: 0.0,
     };
     let vup = Vec {
         x: 0.0,
         y: 1.0,
         z: 0.0,
     };
-    let fd = lf.sub(&[la]).len();
-    let ap = 2.0;
+    let fd = 10.0;
+    let ap = 0.1;
     let cam = camera::camera::init(&lf, &la, &vup, 20.0, aspect_ratio, ap, fd);
 
     let mut f = File::create("img.ppm")?;
@@ -216,6 +264,7 @@ fn main() -> std::io::Result<()> {
 
     for j in (0..image_height).rev() {
         print!("\rscan lines remaining: {j}  ");
+        std::io::stdout().flush().unwrap();
         for i in 0..image_width {
             let mut c = Colour {
                 x: 0.0,
